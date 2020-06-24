ad51cb412567e0cf3f2a156e44cb812af6b90922
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-06-24 17:08:40 -0430

    btn mute

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,11 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const playPause = document.querySelector("#play-pause");
+const onOffSond = document.querySelector("#on-of-sond");
 
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-button.onclick = () => (player.media.paused ? player.play() : player.stop());
+playPause.onclick = () => (player.media.paused ? player.play() : player.stop());
+onOffSond.onclick = () =>
+  player.media.muted ? player.unmute() : player.mute();
