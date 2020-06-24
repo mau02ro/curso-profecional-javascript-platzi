3145710a183b3b6f5566efc217eaeea92dfdb17b
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-06-24 17:06:00 -0430

    clase 07

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,13 +1,28 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  //initPlugins
+  this._initPlugins();
 }
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
 };
-
 MediaPlayer.prototype.stop = function () {
   this.media.pause();
+};
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
 };
 
 export default MediaPlayer;
