91827d5f71bbe8373f9b7a7eea1cf0ed809b64ae
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-06-24 17:56:33 -0430

    this

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,12 @@ const video = document.querySelector("video");
 const playPause = document.querySelector("#play-pause");
 const onOffSond = document.querySelector("#on-of-sond");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    /*new AutoPlay()*/
+  ],
+});
 
 playPause.onclick = () => (player.media.paused ? player.play() : player.stop());
 onOffSond.onclick = () =>
